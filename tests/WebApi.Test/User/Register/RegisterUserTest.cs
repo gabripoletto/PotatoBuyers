faf41d55459448263d0d8f736d5870f1bcd51604
Gabriel <gabriel.poletto@goop.com.br>
2024-07-25 16:42:39 -0300

    xunit no endPoint

---
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -0,0 +1,30 @@
+﻿using CommomTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace WebApi.Test.User.Register
+{
+    public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public RegisterUserTest(WebApplicationFactory<Program> factory) 
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = RegisterUserValidatorBuilder.Build();
+
+            var response = await _httpClient.PostAsJsonAsync("User", request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+    }
+}
+                                       
