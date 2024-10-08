dea5f8a2976d87593c52adb13553343a2044980f
Gabriel <gabriel.poletto@goop.com.br>
2024-10-08 16:12:49 -0300

    Create Login endPoint and exceptions

---
+++ src/Backend/PotatoBuyers.API/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PotatoBuyers.Application.UseCases.Login.DoLogin;
+using PotatoBuyers.Communication.Requests;
+using PotatoBuyers.Communication.Responses;
+
+namespace PotatoBuyers.API.Controllers
+{
+    public class LoginController : PotatoBuyersBaseController
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Login([FromServices] IDoLoginUseCase useCase, [FromBody] RequestLoginJson request)
+        {
+            var response = await useCase.Execute(request);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ src/Shared/PotatoBuyers.Exceptions/ExceptionsBase/InvalidLoginException.cs
@@ -0,0 +1,9 @@
+﻿using PotatoBuyers.Exceptions.ResponsesMessages;
+
+namespace PotatoBuyers.Exceptions.ExceptionsBase
+{
+    public class InvalidLoginException : PotatoBuyersExceptions
+    {
+        public InvalidLoginException() : base(ErrorMessages.EMAIL_OR_PASSWORD_INVALID) { }
+    }
+}
